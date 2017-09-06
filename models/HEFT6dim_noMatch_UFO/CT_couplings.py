b2a569efa3beea55f0915d81d8561137a001be3a
Ludovic Scyboz <scyboz@mpp.mpg.de>
2017-09-06 14:00:26 +0200

    Tidied up

---
+++ models/HEFT6dim_noMatch_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.27
+# Mathematica version: 10.4.1 for Linux x86 (64-bit) (April 11, 2016)
+# Date: Wed 30 Aug 2017 10:48:11
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
